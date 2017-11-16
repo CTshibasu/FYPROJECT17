8c22690aa683eca6f884e03fc95a86c14736a0dd
Corneille Tshibasu <corneilletshibasu@Corneilles-MacBook-Pro.local>
2017-11-16 20:54:55 +0000

    added new file retABC.php for string-matching functionality

---
+++ retABC.php
@@ -0,0 +1,14 @@
+<?php
+	ini_set('display_errors',1);
+	ini_set('display_startup_errors',1);
+	error_reporting(-1);
+
+	// this program will get a substring for the abc notation and return
+	// a list of tunes, the thing is the first tune will always be a precedent to
+	// searching for the best matches
+
+	// firstly, I will program the function to enter in the tune id of a tune,
+	// return it's abc, save as a string
+	function retABC(){
+		
+	}
