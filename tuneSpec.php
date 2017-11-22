54640467611f9f09b6493f6ff37674ac0568ed50
Corneille Tshibasu <corneilletshibasu@Corneilles-MacBook-Pro.local>
2017-11-22 15:35:37 +0000

    added structure for TuneSpec page

--- tuneSpec.php
+++ tuneSpec.php
@@ -7,16 +7,45 @@
 	// firstly, return the tune id of the tune and run the getTuneInfo, alongside that the $abcInfo, with that you can create 
 	// a pie chart of some sorts and also an infrastructure of what that particular set consisted of etc.
 
-	$_GET['id'];
+	// ; // this will hold the id for the tune I query
 
-	// include the header
-	// now to use the include files for the header and the
-	include '/Applications/MAMP/htdocs/SessionCURL/adminUser/pages/inc_header.php';
-	include '/Applications/MAMP/htdocs/SessionCURL/Tunebook.php';
+	$tune_id = $_GET['id'];
 
+	// then run the tuneInfo funciton on the id
+
+	// now run function
+	
+
+	// now to use the include files for the header and the...
+	include '../../Tunebook.php';
+	include '../../tuneSet.php';
+	include 'recRelate.php';
+
+	$res = json_decode(getTuneInfo($tune_id), 1);
+	$title = "Tune: ".$res["name"];
+
+	include 'inc_header.php';
+
+
+	// var_dump test
+	// var_dump($res);
 ?>
 
-
+	<div id="page-wrapper">
+		<div class="row">
+			<div class="col-lg-12">
+				<!-- PAGE HEADER FOR THE TITLE -->
+                <h1 class="page-header"><?=$title?></h1>
+            </div>
+            <!-- may include some extra bits but not required at the moment -->
+           <div class="well" style="display:inline-block; width:650px;">
+			    <div class="container">
+			    	<!-- contain the info on the tune -->
+			    	
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php
 	include '/Applications/MAMP/htdocs/SessionCURL/adminUser/pages/inc_footer.php';	
